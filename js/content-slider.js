6612202f6cb26b9d7e394ac6c1dd5c0b53623753
Jill Ducey <jill@jillducey.com>
2022-09-16 22:46:26 -0500

    Add scroll snap and hook up previous and next buttons

--- js/content-slider.js
+++ js/content-slider.js
@@ -1,50 +1,34 @@
 window.addEventListener(
-  "touchstart",
+  'touchstart',
   function touched() {
-    document.body.classList.add("touch");
-    window.removeEventListener("touchstart", touched, false);
+    document.body.classList.add('touch');
+    window.removeEventListener('touchstart', touched, false);
   },
   false
 );
 
 // Intersection Observer for lazy loading images
-window.addEventListener("load", function () {
-  const galleryContainer = document.querySelector(".gallery-container");
-  galleryContainer.classList.remove("no-js");
-  const galleryImages = document.querySelectorAll(".gallery-container img");
+window.addEventListener('load', function () {
+  const galleryContainer = document.querySelector('.gallery-container');
+  galleryContainer.classList.remove('no-js');
+  const galleryImages = document.querySelectorAll('.gallery-container img');
 
-  if ("IntersectionObserver" in window) {
+  if ('IntersectionObserver' in window) {
     // Create a new observer
-    const observer = new IntersectionObserver(
-      function (images) {
-        images.forEach((image) => {
-          image.target.classList.remove("visible");
-          if (!image.isIntersecting) {
-            return;
-          }
-          image.target.onload = () => image.target.classList.remove("dots");
-          if (image.target.dataset.src) {
-            image.target.setAttribute("src", image.target.dataset.src);
-            image.target.removeAttribute("data-src");
-          }
-          image.target.classList.add("visible");
-
-          // if (image.intersectionRatio < 0.5) {
-          //   image.target.classList.add("alert");
-          // }
-          // if (image.intersectionRatio > 0.5) {
-          //   image.target.classList.add("warning");
-          //   image.target.setAttribute("src", image.target.dataset.src);
-          // }
-          // if (image.intersectionRatio >= 1) {
-          //   image.target.classList.add("success");
-          // }
-        });
-      }
-      // {
-      //   threshold: [0, 0.5, 1.0],
-      // }
-    );
+    const observer = new IntersectionObserver(function (images) {
+      images.forEach((image) => {
+        image.target.classList.remove('visible');
+        if (!image.isIntersecting) {
+          return;
+        }
+        image.target.onload = () => image.target.classList.remove('dots');
+        if (image.target.dataset.src) {
+          image.target.setAttribute('src', image.target.dataset.src);
+          image.target.removeAttribute('data-src');
+        }
+        image.target.classList.add('visible');
+      });
+    });
 
     // Start observing the target element
     galleryImages.forEach((image) => {
@@ -52,17 +36,17 @@ window.addEventListener("load", function () {
     });
   } else {
     galleryImages.forEach((image) => {
-      image.setAttribute("src", image.getAttribute("data-src"));
-      image.classList.remove("dots");
+      image.setAttribute('src', image.getAttribute('data-src'));
+      image.classList.remove('dots');
     });
   }
 });
 
 // Scroll the gallery
-const gallery = document.querySelector(".gallery");
-const galleryControls = document.querySelector(".gallery-controls");
-const slides = document.querySelectorAll(".gallery li");
-galleryControls.classList.remove("visually-hidden");
+const gallery = document.querySelector('.gallery');
+const galleryControls = document.querySelector('.gallery-controls');
+const slides = document.querySelectorAll('.gallery li');
+galleryControls.classList.remove('visually-hidden');
 
 function scrollIt(slideToShow) {
   let slidesArray = Array.from(slides);
@@ -72,22 +56,22 @@ function scrollIt(slideToShow) {
 }
 
 function showSlide(dir) {
-  let visible = document.querySelectorAll(".gallery .visible");
-  let index = dir === "previous" ? 0 : 1;
+  let visible = document.querySelectorAll('.gallery .visible');
+  let index = dir === 'previous' ? 0 : 1;
 
   if (visible.length > 1) {
     scrollIt(visible[index]);
   } else {
     let newSlide =
       index === 0
-        ? visible[0].previousElementSibling
-        : visible[0].nextElementSibling;
+        ? visible[0].parentElement.parentElement.previousElementSibling
+        : visible[0].parentElement.parentElement.nextElementSibling;
     if (newSlide) {
       scrollIt(newSlide);
     }
   }
 }
 
-galleryControls.addEventListener("click", (e) => {
-  showSlide(e.target.closest("button").id);
+galleryControls.addEventListener('click', (e) => {
+  showSlide(e.target.closest('button').id);
 });
